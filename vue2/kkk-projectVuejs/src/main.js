d9076d76cfc9bf977336f87c4dbb9af393ed381c
khanh nguyen <khanh-mac16@khanhs-MacBook-Pro.local>
2024-01-01 07:22:46 +0900

    life cycle in  vue2

--- vue2/kkk-projectVuejs/src/main.js
+++ vue2/kkk-projectVuejs/src/main.js
@@ -4,12 +4,35 @@ import App from './App.vue'
 new Vue({
   el: '#app',
   render: h => h(App),
+  
+  data () {
+    return {
+      stateOfBob: '--stateOfBob--Welcome to Your Vue.js App',
+      test: 'tttttt',
+    }
+  },
   methods: {
     getUnits: function () {
       alert(333);
     }
   },
+  mounted() {
+    console.log("I'm mounted hook:", this.test );
+    
+  },
+  //khanh - life cycle 
+  //beforeCreate() {
+  //   console.log("I'm beforeCreate hook");
+  //   // console.log("Bob is currently ", this.stateOfBob);
+  //   // console.log("computed hook is returning ", this.test);
+  // },
   beforeMount() {
+    console.log("I'm beforeMount ", this.test );
     this.getUnits()
-  }
+  }, 
+  beforeCreate() {
+    console.log("I'm beforeCreate hook", this.test );
+  
+  },
+
 })
